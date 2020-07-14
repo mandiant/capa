ba7c21852e100d3d92af04f4ffa25b311df66d56
Moritz <mr-tz@users.noreply.github.com>
2020-07-14 23:02:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,21 +22,16 @@ with open(os.path.join("capa", "version.py"), "rb") as f:
     exec(f.read())
 
 
-def get_rule_paths():
-    return [os.path.join("..", x[0], "*.yml") for x in os.walk("rules")]
-
-
 setuptools.setup(
     name="capa",
     version=__version__,
-    description="",
+    description="The FLARE team's open-source tool to identify capabilities in executable files.",
     long_description="",
     author="Willi Ballenthin, Moritz Raabe",
     author_email="william.ballenthin@mandiant.com, moritz.raabe@mandiant.com",
     url="https://www.github.com/fireeye/capa",
-    packages=setuptools.find_packages(exclude=["tests", "testbed"]),
+    packages=setuptools.find_packages(exclude=["tests"]),
     package_dir={"capa": "capa"},
-    package_data={"capa": get_rule_paths()},
     entry_points={"console_scripts": ["capa=capa.main:main",]},
     include_package_data=True,
     install_requires=requirements,
