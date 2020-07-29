6d00ae26ae0dec8530afcb74fccbf3f80de91d92
Ana María Martínez Gómez <anamaria.martinezgom@FireEye.com>
2020-07-29 09:55:06 +0200

    hooks: do not repeat #!/usr/bin/env bash
    The comment claims that:
    > doesn't matter if `#!/usr/bin/env bash` gets repeated later on in a hooks file
    
    However it is a convetion that there is only one comment like that and
    the `setup-hooks.sh` script already takes care of adding it if the file
    didn't exist.
    
    Alternatively, we could leave the extra comment and remove the related
    code in `setup-hooks.sh`. Currently two comments are added even if the
    file didn't exist.

--- scripts/setup-hooks.sh
+++ scripts/setup-hooks.sh
@@ -13,11 +13,11 @@ GIT_DIR=$(git rev-parse --show-toplevel);
 cd "$GIT_DIR";
 
 # hooks may exist already (e.g. git-lfs configuration)
-# If the `.git/hooks/$arg` file doesn't exist it, initialize with `#!/bin/sh`
+# If the `.git/hooks/$arg` file doesn't exist it, initialize with `#!/usr/bin/env bash`
 # After that append `scripts/hooks/$arg` and ensure they can be run
 create_hook() {
   if [[ ! -e .git/hooks/$1 ]]; then
-    echo "#!/bin/sh" > ".git/hooks/$1";
+    echo "#!/usr/bin/env bash" > ".git/hooks/$1";
   fi
   cat scripts/hooks/"$1" >> ".git/hooks/$1";
   chmod +x .git/hooks/"$1";
