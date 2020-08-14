de2c3c9800e591b0987b4b04cc0e5af04f0ea972
William Ballenthin <william.ballenthin@fireeye.com>
2020-08-14 09:38:08 -0600

    main: display spinner while generating viv workspace

--- capa/main.py
+++ capa/main.py
@@ -18,6 +18,7 @@ import datetime
 import textwrap
 import collections
 
+import halo
 import tqdm
 import colorama
 
@@ -272,13 +273,14 @@ def get_workspace(path, format, should_save=True):
 def get_extractor_py2(path, format):
     import capa.features.extractors.viv
 
-    vw = get_workspace(path, format, should_save=False)
+    with halo.Halo(text="analyzing program", spinner="simpleDots"):
+        vw = get_workspace(path, format, should_save=False)
 
-    try:
-        vw.saveWorkspace()
-    except IOError:
-        # see #168 for discussion around how to handle non-writable directories
-        logger.info("source directory is not writable, won't save intermediate workspace")
+        try:
+            vw.saveWorkspace()
+        except IOError:
+            # see #168 for discussion around how to handle non-writable directories
+            logger.info("source directory is not writable, won't save intermediate workspace")
 
     return capa.features.extractors.viv.VivisectFeatureExtractor(vw, path)
 
