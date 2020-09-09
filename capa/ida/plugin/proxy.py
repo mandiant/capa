697ec9736e8cd5e5e3077a9491c2cf982a185b9c
Michael Hunhoff <mike.hunhoff@gmail.com>
2020-09-09 12:45:35 -0600

    merge conflicts

--- capa/ida/plugin/proxy.py
+++ capa/ida/plugin/proxy.py
@@ -6,10 +6,6 @@
 #  is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 import six
-<<<<<<< HEAD
-
-=======
->>>>>>> master
 from PyQt5 import QtCore
 from PyQt5.QtCore import Qt
 
