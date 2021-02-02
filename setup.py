55dffd7e99943811e9ac7d9e452fe8466b8898f3
Willi Ballenthin <willi.ballenthin@gmail.com>
2021-02-02 07:54:43 -0700

    setup: bump viv to 0.2.0

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ else:
     # py2
     requirements.append("enum34==1.1.6")  # v1.1.6 is needed by halo 0.0.30 / spinners 0.0.24
     requirements.append("halo==0.0.30")  # halo==0.0.30 is the last version to support py2.7
-    requirements.append("vivisect==0.1.0")
+    requirements.append("vivisect==0.2.0")
     requirements.append("viv-utils")
     requirements.append("networkx==2.2")  # v2.2 is last version supported by Python 2.7
     requirements.append("backports.functools-lru-cache")
