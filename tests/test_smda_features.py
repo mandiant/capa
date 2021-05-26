b3fb46bb1e4d6813a0ca86f93e1f31afdeecd137
William Ballenthin <william.ballenthin@fireeye.com>
2021-05-26 09:53:25 -0600

    pep8

--- tests/test_smda_features.py
+++ tests/test_smda_features.py
@@ -6,11 +6,10 @@
 #  is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 import pytest
-
-import capa.features.file
-
 from fixtures import *
 from fixtures import parametrize
+
+import capa.features.file
 
 
 def smda_parametrize(params, valuess, **kwargs):
