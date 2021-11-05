3a12472be8ff39b33048a34a23f29522496cd9b3
William Ballenthin <william.ballenthin@fireeye.com>
2021-11-05 15:23:34 -0600

    perf: render: show evaluate.feature counter

--- scripts/render-time-profile.sh
+++ scripts/render-time-profile.sh
@@ -1,4 +1,4 @@
-fields=("load FLIRT" "viv analyze" "match functions" "match file" "find capabilities");
+fields=("load FLIRT" "viv analyze" "match functions" "match file" "find capabilities" "evaluate.feature");
 
 echo -n "|         |";
 for T in "${fields[@]}"; do
@@ -16,8 +16,8 @@ for rev in "$@"; do
 
     echo -n "| $rev |";
     for T in "${fields[@]}"; do
-        V1=$(cat scripts/perf/capa-$rev-PMA0101.txt | grep "$T" | sed -e "s/^.*$T: //g");
-        V2=$(cat scripts/perf/capa-$rev-k32.txt | grep "$T" | sed -e "s/^.*$T: //g");
+        V1=$(cat scripts/perf/capa-$rev-PMA0101.txt | grep "$T: " | sed -e "s/^.*$T: //g");
+        V2=$(cat scripts/perf/capa-$rev-k32.txt | grep "$T: " | sed -e "s/^.*$T: //g");
         printf ' %-17s |' "$V1/$V2";
     done
     echo "";
