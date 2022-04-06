93a94f9efa738d57d002b037184f188cdc5b1d16
Moritz <mr-tz@users.noreply.github.com>
2022-04-06 11:24:05 +0200

    feat: start dotnet detection (#955)
    * feat: start dotnet detection
    
    * Apply suggestions from code review
    
    Co-authored-by: Willi Ballenthin <willi.ballenthin@gmail.com>
    
    * refactor: dn instead of dotnet
    
    * refactor: format branches, extractor reorg
    
    * refactor: format selection and dotnet detect
    
    * feat: get format, arch, os
    
    * refactor: log errors and exceptions
    
    * ci: also test and build for dotnet-main dev
    
    * fix: import path
    
    * fix: circular dep
    
    * fix: remove buf argument
    feat: get runtime meta data
    
    * fix: log unsupported runtime error
    
    * fix: type ignore
    
    Co-authored-by: Willi Ballenthin <willi.ballenthin@gmail.com>

--- tests/test_dotnet_features.py
+++ tests/test_dotnet_features.py
@@ -6,14 +6,10 @@
 #  is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-# b9f5bd514485fb06da39beff051b9fdc
-
 import pytest
 import fixtures
 from fixtures import *
 from fixtures import parametrize
-
-import capa.features.file
 
 
 @parametrize(
