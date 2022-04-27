8f1d04f94ed19096488e4f216b656f11cfbe3012
William Ballenthin <william.ballenthin@fireeye.com>
2022-04-27 08:28:49 -0600

    version: add accessor for capa major version and git checkout command

--- capa/version.py
+++ capa/version.py
@@ -1,1 +1,13 @@
 __version__ = "3.2.0"
+
+
+def get_major_version():
+    return int(__version__.partition(".")[0])
+
+
+def get_rules_branch():
+    return f"v{get_major_version()}"
+
+
+def get_rules_checkout_command():
+    return f"$ git clone https://github.com/mandiant/capa-rules.git -b {get_rules_branch()} /local/path/to/rules"
