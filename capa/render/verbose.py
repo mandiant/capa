cb6781a143ebf015b2857a2152ac4b6862fa6ce1
Willi Ballenthin <willi.ballenthin@gmail.com>
2022-06-28 11:11:00 -0600

    verbose: remove extraneous import

--- capa/render/verbose.py
+++ capa/render/verbose.py
@@ -22,8 +22,6 @@ Unless required by applicable law or agreed to in writing, software distributed 
  is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and limitations under the License.
 """
-from tarfile import FIFOTYPE
-
 import tabulate
 import dnfile.mdtable
 import dncil.clr.token
