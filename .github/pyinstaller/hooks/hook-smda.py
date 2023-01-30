bf7c569060fba22b59d93490fe59bb043e9adc1e
Moritz <mr-tz@users.noreply.github.com>
2023-01-30 10:15:56 +0100

    Delete hook-smda.py (#1296)

--- .github/pyinstaller/hooks/hook-smda.py
+++
@@ -1,5 +0,0 @@
-# Copyright (C) 2020 Mandiant, Inc. All Rights Reserved.
-import PyInstaller.utils.hooks
-
-# ref: https://groups.google.com/g/pyinstaller/c/amWi0-66uZI/m/miPoKfWjBAAJ
-binaries = PyInstaller.utils.hooks.collect_dynamic_libs("capstone")
