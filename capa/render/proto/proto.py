a7e4d265e264cff14609a8fc9a1b3084d41c38b1
mr-tz <moritz.raabe@mandiant.com>
2023-03-08 14:45:26 +0100

    convert rd meta to proto

---
+++ capa/render/proto/proto.py
@@ -0,0 +1,120 @@
+import sys
+import json
+
+import capa.features.freeze
+import capa.render.proto.capa_pb2
+import capa.render.result_document
+from capa.features.freeze import AddressType
+
+
+def main():
+    # first compile protobuf
+    # protoc.exe --python_out . capa/render/proto/capa.proto
+
+    fpath = sys.argv[1]
+    with open(fpath, "r", encoding="utf-8") as f:
+        fdata = f.read()
+
+    doc = capa.render.result_document.ResultDocument.parse_obj(json.loads(fdata))
+
+    p = to_proto(doc)
+
+    print(p)
+
+
+def to_proto(doc):
+    m = metadata_from_capa(doc.meta)
+    return m
+
+
+def metadata_from_capa(meta: capa.render.result_document.Metadata) -> capa.render.proto.capa_pb2.Metadata:
+    m = capa.render.proto.capa_pb2.Metadata()
+
+    m.timestamp = str(meta.timestamp)  # TODO google.protobuf.timestamp_pb2.Timestamp?
+    m.version = meta.version
+    m.argv.extend(meta.argv)
+
+    m.sample.md5 = meta.sample.md5
+    m.sample.sha1 = meta.sample.sha1
+    m.sample.sha256 = meta.sample.sha256
+    m.sample.path = meta.sample.path
+
+    m.analysis.format = meta.analysis.format
+    m.analysis.arch = meta.analysis.arch
+    m.analysis.os = meta.analysis.os
+    m.analysis.extractor = meta.analysis.extractor
+    m.analysis.rules.extend(meta.analysis.rules)
+    m.analysis.base_address.CopyFrom(addr_from_freeze(meta.analysis.base_address))
+
+    m.analysis.layout.CopyFrom(
+        capa.render.proto.capa_pb2.Layout(
+            functions=[
+                capa.render.proto.capa_pb2.FunctionLayout(
+                    address=addr_from_freeze(func.address),
+                    matched_basic_blocks=[
+                        capa.render.proto.capa_pb2.BasicBlockLayout(address=addr_from_freeze(bb.address))
+                        for bb in func.matched_basic_blocks
+                    ],
+                )
+                for func in meta.analysis.layout.functions
+            ]
+        )
+    )
+
+    m.analysis.feature_counts.file = meta.analysis.feature_counts.file
+    m.analysis.feature_counts.functions.extend(
+        [
+            capa.render.proto.capa_pb2.FunctionFeatureCount(address=addr_from_freeze(ffc.address), count=ffc.count)
+            for ffc in meta.analysis.feature_counts.functions
+        ]
+    )
+    m.analysis.library_functions.extend(
+        [
+            capa.render.proto.capa_pb2.LibraryFunction(address=addr_from_freeze(lf.address), name=lf.name)
+            for lf in meta.analysis.library_functions
+        ]
+    )
+
+    return m
+
+
+def addr_from_freeze(a: capa.features.freeze.Address) -> capa.render.proto.capa_pb2.Address:
+    address = capa.render.proto.capa_pb2.Address()
+    if a.type is AddressType.ABSOLUTE:
+        address.type = capa.render.proto.capa_pb2.AddressType.ADDRESSTYPE_ABSOLUTE
+        address.v0.CopyFrom(capa.render.proto.capa_pb2.Integer(u=a.value))
+        return address
+
+    elif a.type is AddressType.RELATIVE:
+        address.type = capa.render.proto.capa_pb2.AddressType.ADDRESSTYPE_RELATIVE
+        address.v0.CopyFrom(capa.render.proto.capa_pb2.Integer(u=a.value))
+        return address
+
+    elif a.type is AddressType.FILE:
+        address.type = capa.render.proto.capa_pb2.AddressType.ADDRESSTYPE_FILE
+        address.v0.CopyFrom(capa.render.proto.capa_pb2.Integer(u=a.value))
+        return address
+
+    elif a.type is AddressType.DN_TOKEN:
+        address.type = capa.render.proto.capa_pb2.AddressType.ADDRESSTYPE_DN_TOKEN
+        address.v0.u = a.value  # TODO or v0.CopyFrom(Integer(a.value))?
+        return address
+
+    elif a.type is AddressType.DN_TOKEN_OFFSET:
+        token, offset = a.value
+        address.type = capa.render.proto.capa_pb2.AddressType.ADDRESSTYPE_DN_TOKEN_OFFSET
+        address.v1.v0.CopyFrom(capa.render.proto.capa_pb2.Integer(u=token))
+        address.v1.v1.CopyFrom(capa.render.proto.capa_pb2.Integer(u=offset))
+        return address
+
+    elif a.type is AddressType.NO_ADDRESS:
+        address.type = capa.render.proto.capa_pb2.AddressType.ADDRESSTYPE_NO_ADDRESS
+        # value == None so just don't set here
+        return address
+
+    else:
+        raise NotImplementedError(f"type {a.type} not implemented")
+
+
+if __name__ == "__main__":
+    main()
--- tests/test_proto.py
+++ tests/test_proto.py
@@ -10,20 +10,21 @@ import pathlib
 import subprocess
 
 import pydantic
-
-from fixtures import *
-
 import capa.render
 import capa.render.proto
 import capa.render.utils
 import capa.features.freeze
+import capa.features.address
+import capa.render.proto.proto
 import capa.render.proto.capa_pb2
 import capa.render.result_document
 import capa.features.freeze.features
+from fixtures import *
 from capa.render.result_document import ResultDocument
 
 
-def test_generate_proto(tmp_path: pathlib.Path):
+# TODO enable/remove
+def _test_generate_proto(tmp_path: pathlib.Path):
     tmp_path.mkdir(exist_ok=True, parents=True)
     proto_path = tmp_path / "capa.proto"
     json_path = tmp_path / "capa.json"
@@ -60,6 +61,85 @@ def test_generate_proto(tmp_path: pathlib.Path):
 def test_translate_to_proto(pma0101_rd: ResultDocument):
     src = pma0101_rd
 
-    dst = capa.render.proto.capa_pb2.ResultDocument()
+    meta = src.meta
+    dst = capa.render.proto.proto.metadata_from_capa(meta)
 
-    assert True
+    assert str(meta.timestamp) == dst.timestamp  # TODO type?
+    assert meta.version == dst.version
+    assert list(meta.argv) == dst.argv
+
+    assert meta.sample.md5 == dst.sample.md5
+    assert meta.sample.sha1 == dst.sample.sha1
+    assert meta.sample.sha256 == dst.sample.sha256
+    assert meta.sample.path == dst.sample.path
+
+    assert meta.analysis.format == dst.analysis.format
+    assert meta.analysis.arch == dst.analysis.arch
+    assert meta.analysis.os == dst.analysis.os
+    assert meta.analysis.extractor == dst.analysis.extractor
+    assert list(meta.analysis.rules) == dst.analysis.rules
+    assert capa.render.proto.proto.addr_from_freeze(meta.analysis.base_address) == dst.analysis.base_address
+
+    assert len(meta.analysis.layout.functions) == len(dst.analysis.layout.functions)
+    # TODO use zip()
+    for i, f in enumerate(meta.analysis.layout.functions):
+        assert capa.render.proto.proto.addr_from_freeze(f.address) == dst.analysis.layout.functions[i].address
+
+        assert len(f.matched_basic_blocks) == len(dst.analysis.layout.functions[i].matched_basic_blocks)
+        for j, bb in enumerate(f.matched_basic_blocks):
+            assert (
+                capa.render.proto.proto.addr_from_freeze(bb.address)
+                == dst.analysis.layout.functions[i].matched_basic_blocks[j].address
+            )
+
+    assert meta.analysis.feature_counts.file == dst.analysis.feature_counts.file
+    assert len(meta.analysis.feature_counts.functions) == len(dst.analysis.feature_counts.functions)
+    for rd_f, proto_f in zip(meta.analysis.feature_counts.functions, dst.analysis.feature_counts.functions):
+        assert capa.render.proto.proto.addr_from_freeze(rd_f.address) == proto_f.address
+        assert rd_f.count == proto_f.count
+
+    assert len(meta.analysis.library_functions) == len(dst.analysis.library_functions)
+    for rd_lf, proto_lf in zip(meta.analysis.library_functions, dst.analysis.library_functions):
+        assert capa.render.proto.proto.addr_from_freeze(rd_lf.address) == proto_lf.address
+        assert rd_lf.name == proto_lf.name
+
+
+def test_addr_from_freeze():
+    a = capa.features.address.AbsoluteVirtualAddress(0x400000)
+    a = capa.features.freeze.Address.from_capa(a)
+    a = capa.render.proto.proto.addr_from_freeze(a)
+    assert a.type == capa.render.proto.capa_pb2.ADDRESSTYPE_ABSOLUTE
+    assert a.v0.u == 0x400000
+
+    a = capa.features.address.RelativeVirtualAddress(0x100)
+    a = capa.features.freeze.Address.from_capa(a)
+    a = capa.render.proto.proto.addr_from_freeze(a)
+    assert a.type == capa.render.proto.capa_pb2.ADDRESSTYPE_RELATIVE
+    assert a.v0.u == 0x100
+
+    a = capa.features.address.FileOffsetAddress(0x200)
+    a = capa.features.freeze.Address.from_capa(a)
+    a = capa.render.proto.proto.addr_from_freeze(a)
+    assert a.type == capa.render.proto.capa_pb2.ADDRESSTYPE_FILE
+    assert a.v0.u == 0x200
+
+    a = capa.features.address.DNTokenAddress(0x123456)
+    a = capa.features.freeze.Address.from_capa(a)
+    a = capa.render.proto.proto.addr_from_freeze(a)
+    assert a.type == capa.render.proto.capa_pb2.ADDRESSTYPE_DN_TOKEN
+    assert a.v0.u == 0x123456
+
+    a = capa.features.address.DNTokenOffsetAddress(0x123456, 0x10)
+    a = capa.features.freeze.Address.from_capa(a)
+    a = capa.render.proto.proto.addr_from_freeze(a)
+    assert a.type == capa.render.proto.capa_pb2.ADDRESSTYPE_DN_TOKEN_OFFSET
+    assert a.v1.v0.u == 0x123456
+    assert a.v1.v1.u == 0x10
+
+    a = capa.features.address._NoAddress()
+    a = capa.features.freeze.Address.from_capa(a)
+    a = capa.render.proto.proto.addr_from_freeze(a)
+    assert a.type == capa.render.proto.capa_pb2.ADDRESSTYPE_NO_ADDRESS
+
+
+# TODO proto to RD?
