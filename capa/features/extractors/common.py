5a1009520df91b278a8ff63dab150430c12377d7
Pratham Chauhan <Prathamchauhan2002@gmail.com>
2023-03-29 16:10:44 +0530

    Revert "Revert "introducing match strings constant for formats""
    This reverts commit b49fb7fcf94b9ae2259b657bd47c0ee250ee4373.

--- capa/features/extractors/common.py
+++ capa/features/extractors/common.py
@@ -16,6 +16,10 @@ from capa.features.address import NO_ADDRESS, Address, FileOffsetAddress
 
 logger = logging.getLogger(__name__)
 
+#match strings for formats
+MATCH_PE = b"MZ"
+MATCH_ELF = b"\x7fELF"
+MATCH_RESULT = b"{\"meta\":"
 
 def extract_file_strings(buf, **kwargs) -> Iterator[Tuple[String, Address]]:
     """
@@ -29,13 +33,13 @@ def extract_file_strings(buf, **kwargs) -> Iterator[Tuple[String, Address]]:
 
 
 def extract_format(buf) -> Iterator[Tuple[Feature, Address]]:
-    if buf.startswith(b"MZ"):
+    if buf.startswith(MATCH_PE):
         yield Format(FORMAT_PE), NO_ADDRESS
-    elif buf.startswith(b"\x7fELF"):
+    elif buf.startswith(MATCH_ELF):
         yield Format(FORMAT_ELF), NO_ADDRESS
     elif is_freeze(buf):
         yield Format(FORMAT_FREEZE), NO_ADDRESS
-    elif buf.startswith(b"{\"meta\":"):
+    elif buf.startswith(MATCH_RESULT):
         yield Format(FORMAT_RESULT), NO_ADDRESS
     else:
         # we likely end up here:
@@ -47,14 +51,14 @@ def extract_format(buf) -> Iterator[Tuple[Feature, Address]]:
 
 
 def extract_arch(buf) -> Iterator[Tuple[Feature, Address]]:
-    if buf.startswith(b"MZ"):
+    if buf.startswith(MATCH_PE):
         yield from capa.features.extractors.pefile.extract_file_arch(pe=pefile.PE(data=buf))
 
-    elif buf.startswith(b"\x7fELF"):
+    elif buf.startswith(MATCH_ELF):
         with contextlib.closing(io.BytesIO(buf)) as f:
             arch = capa.features.extractors.elf.detect_elf_arch(f)
     
-    elif buf.startswith(b"{\"meta\":"):
+    elif buf.startswith(MATCH_RESULT):
         arch = ARCH_ANY
 
         if arch not in capa.features.common.VALID_ARCH:
@@ -79,11 +83,11 @@ def extract_arch(buf) -> Iterator[Tuple[Feature, Address]]:
 
 
 def extract_os(buf) -> Iterator[Tuple[Feature, Address]]:
-    if buf.startswith(b"MZ"):
+    if buf.startswith(MATCH_PE):
         yield OS(OS_WINDOWS), NO_ADDRESS
-    elif buf.startswith(b"{\"meta\":"):
+    elif buf.startswith(MATCH_RESULT):
         yield OS(OS_ANY), NO_ADDRESS
-    elif buf.startswith(b"\x7fELF"):
+    elif buf.startswith(MATCH_ELF):
         with contextlib.closing(io.BytesIO(buf)) as f:
             os = capa.features.extractors.elf.detect_elf_os(f)
 
