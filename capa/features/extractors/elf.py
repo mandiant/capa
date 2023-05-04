a62996420f6504c429d19307cf6de7f84d027fbd
Aayush Goel <81844215+Aayush-Goel-04@users.noreply.github.com>
2023-05-04 18:29:15 +0530

    Update elf.py
    corrected pre-formatted strings

--- capa/features/extractors/elf.py
+++ capa/features/extractors/elf.py
@@ -857,56 +857,56 @@ def detect_elf_os(f) -> str:
     try:
         elf = ELF(f)
     except Exception as e:
-        logger.debug(f"Error parsing ELF file: {str(e)}")
+        logger.warning("Error parsing ELF file: %s", e)
         return "unknown"
 
     try:
         osabi_guess = guess_os_from_osabi(elf)
         logger.debug("guess: osabi: %s", osabi_guess)
     except Exception as e:
-        logger.debug(f"Error guessing OS from OSABI: {str(e)}")
+        logger.warning("Error guessing OS from OSABI: %s", e)
         osabi_guess = None
 
     try:
         ph_notes_guess = guess_os_from_ph_notes(elf)
         logger.debug("guess: ph notes: %s", ph_notes_guess)
     except Exception as e:
-        logger.debug(f"Error guessing OS from program header notes: {str(e)}")
+        logger.warning("Error guessing OS from program header notes: %s", e)
         ph_notes_guess = None
 
     try:
         sh_notes_guess = guess_os_from_sh_notes(elf)
         logger.debug("guess: sh notes: %s", sh_notes_guess)
     except Exception as e:
-        logger.debug(f"Error guessing OS from section header notes: {str(e)}")
+        logger.warning("Error guessing OS from section header notes: %s", e)
         sh_notes_guess = None
 
     try:
         linker_guess = guess_os_from_linker(elf)
         logger.debug("guess: linker: %s", linker_guess)
     except Exception as e:
-        logger.debug(f"Error guessing OS from linker: {str(e)}")
+        logger.warning("Error guessing OS from linker: %s", e)
         linker_guess = None
 
     try:
         abi_versions_needed_guess = guess_os_from_abi_versions_needed(elf)
         logger.debug("guess: ABI versions needed: %s", abi_versions_needed_guess)
     except Exception as e:
-        logger.debug(f"Error guessing OS from ABI versions needed: {str(e)}")
+        logger.warning("Error guessing OS from ABI versions needed: %s", e)
         abi_versions_needed_guess = None
 
     try:
         needed_dependencies_guess = guess_os_from_needed_dependencies(elf)
         logger.debug("guess: needed dependencies: %s", needed_dependencies_guess)
     except Exception as e:
-        logger.debug(f"Error guessing OS from needed dependencies: {str(e)}")
+        logger.warning("Error guessing OS from needed dependencies: %s", e)
         needed_dependencies_guess = None
 
     try:
         symtab_guess = guess_os_from_symtab(elf)
         logger.debug("guess: pertinent symbol name: %s", symtab_guess)
     except Exception as e:
-        logger.debug(f"Error guessing OS from symbol table: {str(e)}")
+        logger.warning("Error guessing OS from symbol table: %s", e)
         symtab_guess = None
 
     ret = None
