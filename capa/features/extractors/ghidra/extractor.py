348741db5688646f84ab43718b2877d12425dd9a
colton-gabertan <gabertan.colton@gmail.com>
2023-06-20 03:50:30 -0700

    Update GhidraFeatureExtractor

--- capa/features/extractors/ghidra/extractor.py
+++ capa/features/extractors/ghidra/extractor.py
@@ -11,5 +11,15 @@ class GhidraFeatureExtractor(FeatureExtractor):
     def __init__(self):
         super().__init__()
         self.global_features: List[Tuple[Feature, Address]] = []
+        self.global_features.extend(capa.features.extractors.ghidra.file.extract_file_format())
         self.global_features.extend(capa.features.extractors.ghidra.global_.extract_os())
         self.global_features.extend(capa.features.extractors.ghidra.global_.extract_arch())
+
+    def get_base_address(self):
+        return AbsoluteVirtualAddress(currentProgram.getImageBase().getOffset())
+
+    def extract_global_features(self):
+        yield from self.global_features
+
+    def extract_file_features(self):
+        yield from capa.features.extractors.ghidra.file.extract_features()
