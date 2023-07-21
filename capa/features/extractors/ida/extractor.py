3ab3c61d5ec51639cb5712326d37fda06e46d0c4
Yacine Elhamer <elhamer.yacine@gmail.com>
2023-07-21 13:14:35 +0100

    use ida's hash-extraction functions

--- capa/features/extractors/ida/extractor.py
+++ capa/features/extractors/ida/extractor.py
@@ -35,7 +35,9 @@ class IdaFeatureExtractor(StaticFeatureExtractor):
         self.global_features.extend(capa.features.extractors.ida.file.extract_file_format())
         self.global_features.extend(capa.features.extractors.ida.global_.extract_os())
         self.global_features.extend(capa.features.extractors.ida.global_.extract_arch())
-        self.sample_hashes = SampleHashes.from_bytes(Path(idaapi.get_input_file_path()).read_bytes())
+        self.sample_hashes = SampleHashes(
+            md5=idaapi.get_input_file_md5(), sha1=idaapi.get_input_file_sha1(), sha256=idaapi.get_input_file_sha256()
+        )
 
     def get_base_address(self):
         return AbsoluteVirtualAddress(idaapi.get_imagebase())
