559f2fd162fee9a0e287ca47bfbc2971cbb76a3a
Yacine Elhamer <elhamer.yacine@gmail.com>
2023-10-11 11:56:49 +0200

    cape/file.py: flake8 fixes

--- capa/features/extractors/cape/file.py
+++ capa/features/extractors/cape/file.py
@@ -33,7 +33,9 @@ def get_processes(report: CapeReport) -> Iterator[ProcessHandle]:
             seen_processes[addr] = [process]
         else:
             logger.warning(
-                f"pid and ppid reuse detected between process {process} and process{'es' if len(seen_processes[addr]) > 1 else ''}: {seen_processes[addr]}"
+                "pid and ppid reuse detected between process " + process + " and process" + "es"
+                if len(seen_processes[addr]) > 1
+                else "" + ": " + seen_processes[addr]
             )
             seen_processes[addr].append(process)
 
