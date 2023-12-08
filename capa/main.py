525908dd74e723bec2338b2382d7eb9acac95c24
aaronatp <58194911+aaronatp@users.noreply.github.com>
2023-12-08 03:55:33 -0600

    Edited method for disabling mypy warning

--- capa/main.py
+++ capa/main.py
@@ -1038,7 +1038,7 @@ def main(argv: Optional[List[str]] = None):
     install_common_args(parser, {"sample", "format", "backend", "os", "signatures", "rules", "tag"})
     parser.add_argument("-j", "--json", action="store_true", help="emit JSON instead of text")
     args = parser.parse_args(args=argv)
-    if not args.debug:  # type: ignore[1]
+    if not args.debug:  # type: ignore[assignment]
         sys.excepthook = simple_message_exception_handler
     ret = handle_common_args(args)
     if ret is not None and ret != 0:
