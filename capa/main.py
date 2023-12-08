32a73606153bc5fb337053989444bb64de646a47
aaronatp <58194911+aaronatp@users.noreply.github.com>
2023-12-08 04:37:12 -0600

    Update statement to disable mypy error

--- capa/main.py
+++ capa/main.py
@@ -984,9 +984,9 @@ def simple_message_exception_handler(exctype, value: BaseException, traceback: T
     prints friendly message on unhandled exceptions to regular users (debug mode shows regular stack trace)
 
     args:
-      # TODO(aaronatp): Once capa drops support for Python 3.8, move the exctype type annotation to the function parameters
-      # and remove the "# type: ignore" from the relevant place in the main function
-      # see (https://github.com/mandiant/capa/issues/1896)
+      # TODO(aaronatp): Once capa drops support for Python 3.8, move the exctype type annotation to the
+      # function parameters and remove the "# type: ignore[incompatible-types]" from the relevant place
+      # in the main function, see (https://github.com/mandiant/capa/issues/1896)
       exctype (type[BaseException]): exception class
     """
 
@@ -1040,7 +1040,7 @@ def main(argv: Optional[List[str]] = None):
     parser.add_argument("-j", "--json", action="store_true", help="emit JSON instead of text")
     args = parser.parse_args(args=argv)
     if not args.debug:
-        sys.excepthook = simple_message_exception_handler  # type: ignore
+        sys.excepthook = simple_message_exception_handler  # type: ignore[incompatible-types]
     ret = handle_common_args(args)
     if ret is not None and ret != 0:
         return ret
