5e60b1e501f324f9e9bc85703c811b06c0d3f858
aaronatp <58194911+aaronatp@users.noreply.github.com>
2023-12-22 14:30:06 -0600

    Update json.py

--- capa/render/json.py
+++ capa/render/json.py
@@ -5,10 +5,21 @@
 # Unless required by applicable law or agreed to in writing, software distributed under the License
 #  is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
+from typing import List, Tuple, Iterator, Optional
+
 import capa.render.result_document as rd
 from capa.rules import RuleSet
 from capa.engine import MatchResults
+from capa.features.extractors.base_extractor import CallHandle, ThreadHandle, ProcessHandle
 
 
-def render(meta, rules: RuleSet, capabilities: MatchResults) -> str:
-    return rd.ResultDocument.from_capa(meta, rules, capabilities).model_dump_json(exclude_none=True)
+def render(
+    meta,
+    rules: RuleSet,
+    capabilities: MatchResults,
+    strings: Optional[List[str]],
+    sandbox_data: Optional[Tuple[Iterator[ProcessHandle], Iterator[ThreadHandle], Iterator[CallHandle]]],
+) -> str:
+    return rd.ResultDocument.from_capa(meta, rules, capabilities, strings, sandbox_data).model_dump_json(
+        exclude_none=True
+    )
