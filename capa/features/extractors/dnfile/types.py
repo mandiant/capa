c3b8e2627ca3992793d39c3b4c5742ea034aa5d5
Blas <25017260+bkojusner@users.noreply.github.com>
2024-01-04 13:54:41 -0500

    Linting update types.py

--- capa/features/extractors/dnfile/types.py
+++ capa/features/extractors/dnfile/types.py
@@ -6,8 +6,7 @@
 #  is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-from typing import Optional
-from typing import Tuple
+from typing import Tuple, Optional
 
 class DnType:
     def __init__(self, token: int, class_: Tuple[str, ...], namespace: str = "", member: str = "", access: Optional[str] = None):
