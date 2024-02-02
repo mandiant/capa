5ea8826f2b85f2ba1deb565fac3f1dba50019762
Willi Ballenthin <willi.ballenthin@gmail.com>
2024-02-02 12:01:19 +0000

    binexport: learn to extract function names

--- capa/features/extractors/binexport2/function.py
+++ capa/features/extractors/binexport2/function.py
@@ -7,8 +7,10 @@
 # See the License for the specific language governing permissions and limitations under the License.
 from typing import Tuple, Iterator
 
+from capa.features.file import FunctionName
 from capa.features.common import Feature
 from capa.features.address import Address
+from capa.features.extractors.binexport2 import FunctionContext
 from capa.features.extractors.base_extractor import FunctionHandle
 
 
@@ -28,8 +30,25 @@ def extract_recursive_call(fh: FunctionHandle):
 
 
 def extract_function_name(fh: FunctionHandle):
-    # TODO(wb): 1755
-    yield from ()
+    fhi: FunctionContext = fh.inner
+
+    be2 = fhi.ctx.be2
+    idx = fhi.ctx.idx
+    analysis = fhi.ctx.analysis
+
+    flow_graph_index = fhi.flow_graph_index
+
+    flow_graph_address = idx.flow_graph_address_by_index[flow_graph_index]
+    vertex_index = idx.vertex_index_by_address[flow_graph_address]
+    vertex = be2.call_graph.vertex[vertex_index]
+
+    if vertex.HasField("mangled_name"):
+        yield FunctionName(vertex.mangled_name), fh.address
+    elif flow_graph_address in analysis.thunks:
+        thunk_vertex_index = analysis.thunks[flow_graph_address]
+        thunk_vertex = be2.call_graph.vertex[thunk_vertex_index]
+        if thunk_vertex.HasField("mangled_name"):
+            yield FunctionName(thunk_vertex.mangled_name), fh.address
 
 
 def extract_features(fh: FunctionHandle) -> Iterator[Tuple[Feature, Address]]:
