1975b6455c9e5ccfdf295210659730c1cfc50d25
ygasparis <gasparis@google.com>
2024-06-18 11:38:02 -0700

    extract import / export symbols from stripped elf binaries (#2142)

--- tests/test_elffile_features.py
+++ tests/test_elffile_features.py
@@ -14,17 +14,11 @@ from capa.features.extractors.elffile import extract_file_export_names, extract_
 
 CD = Path(__file__).resolve().parent
 SAMPLE_PATH = CD / "data" / "055da8e6ccfe5a9380231ea04b850e18.elf_"
+STRIPPED_SAMPLE_PATH = CD / "data" / "bb38149ff4b5c95722b83f24ca27a42b.elf_"
 
 
-def test_elffile_import_features():
-    expected_imports = [
-        "memfrob",
-        "puts",
-        "__libc_start_main",
-        "malloc",
-        "__cxa_finalize",
-    ]
-    path = Path(SAMPLE_PATH)
+def check_import_features(sample_path, expected_imports):
+    path = Path(sample_path)
     elf = ELFFile(io.BytesIO(path.read_bytes()))
     # Extract imports
     imports = list(extract_file_import_names(elf))
@@ -38,6 +32,52 @@ def test_elffile_import_features():
     # Check if all expected symbol names are found
     for symbol_name in expected_imports:
         assert symbol_name in extracted_symbol_names, f"Symbol '{symbol_name}' not found in imports."
+
+
+def check_export_features(sample_path, expected_exports):
+    path = Path(sample_path)
+    elf = ELFFile(io.BytesIO(path.read_bytes()))
+    # Extract imports
+    exports = list(extract_file_export_names(elf))
+
+    # Verify that at least one export was found
+    assert len(exports) > 0, "No exports were found."
+
+    # Extract the symbol names from the extracted imports
+    extracted_symbol_names = [exported[0].value for exported in exports]
+
+    # Check if all expected symbol names are found
+    for symbol_name in expected_exports:
+        assert symbol_name in extracted_symbol_names, f"Symbol '{symbol_name}' not found in exports."
+
+
+def test_stripped_elffile_import_features():
+    expected_imports = ["__cxa_atexit", "__cxa_finalize", "__stack_chk_fail", "fclose", "fopen", "__android_log_print"]
+    check_import_features(STRIPPED_SAMPLE_PATH, expected_imports)
+
+
+def test_stripped_elffile_export_features():
+    expected_exports = [
+        "_ZN7_JNIEnv14GetArrayLengthEP7_jarray",
+        "Java_o_ac_a",
+        "Java_o_ac_b",
+        "_Z6existsPKc",
+        "_ZN7_JNIEnv17GetStringUTFCharsEP8_jstringPh",
+        "_ZN7_JNIEnv21GetObjectArrayElementEP13_jobjectArrayi",
+        "_ZN7_JNIEnv21ReleaseStringUTFCharsEP8_jstringPKc",
+    ]
+    check_export_features(STRIPPED_SAMPLE_PATH, expected_exports)
+
+
+def test_elffile_import_features():
+    expected_imports = [
+        "memfrob",
+        "puts",
+        "__libc_start_main",
+        "malloc",
+        "__cxa_finalize",
+    ]
+    check_import_features(SAMPLE_PATH, expected_imports)
 
 
 def test_elffile_export_features():
@@ -55,17 +95,4 @@ def test_elffile_export_features():
         "_IO_stdin_used",
         "__libc_csu_init",
     ]
-    path = Path(SAMPLE_PATH)
-    elf = ELFFile(io.BytesIO(path.read_bytes()))
-    # Extract imports
-    exports = list(extract_file_export_names(elf))
-
-    # Verify that at least one export was found
-    assert len(exports) > 0, "No exports were found."
-
-    # Extract the symbol names from the extracted imports
-    extracted_symbol_names = [exported[0].value for exported in exports]
-
-    # Check if all expected symbol names are found
-    for symbol_name in expected_exports:
-        assert symbol_name in extracted_symbol_names, f"Symbol '{symbol_name}' not found in exports."
+    check_export_features(SAMPLE_PATH, expected_exports)
