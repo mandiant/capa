e0cc67ef1a659131770d559e06b8cf7f5e80fd0f
r-sm2024 <moonsunghyun96@gmail.com>
2024-06-18 21:29:57 +0000

    Add VMRayanalysis model and call parser

--- capa/features/extractors/vmray/call.py
+++ capa/features/extractors/vmray/call.py
@@ -1,23 +1,12 @@
-# Copyright (C) 2024 Mandiant, Inc. All Rights Reserved.
-# Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at: [package root]/LICENSE.txt
-# Unless required by applicable law or agreed to in writing, software distributed under the License
-#  is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and limitations under the License.
 import logging
-from typing import Iterator, Tuple
+from typing import Tuple, Iterator
 
-from capa.features.address import Address
-from capa.features.common import Feature, String
-from capa.features.extractors.base_extractor import (
-    CallHandle,
-    ProcessHandle,
-    ThreadHandle,
-)
-from capa.features.extractors.vmray.models import FunctionCall, Param
-from capa.features.insn import API, Number
 from capa.helpers import assert_never
+from capa.features.insn import API, Number
+from capa.features.common import String, Feature
+from capa.features.address import Address
+from capa.features.extractors.vmray.models import FunctionCall, Analysis
+from capa.features.extractors.base_extractor import CallHandle, ThreadHandle, ProcessHandle
 
 logger = logging.getLogger(__name__)
 
@@ -29,84 +18,40 @@ def extract_function_calls(ph: ProcessHandle, th: ThreadHandle, ch: CallHandle) 
 
     args:
       call: FunctionCall object representing the XML fncall element
-
+      
       yields: Feature, address; where Feature is either: API, Number, or String.
     """
 
-    # TODO (meh): update for new models https://github.com/mandiant/capa/issues/2148
-    # print(ch)
-
     # Extract API name
-    yield API(FunctionCall.fncall_id), Address(FunctionCall.addr)
-    
-    # Map str "type" values as int if they are in list
-    datatype = [
-        "unknown", 
-        "void", 
-        "bool", 
-        "signed_8bit", 
-        "unsigned_8bit", 
-        "signed_16bit", 
-        "unsigned_16bit", 
-        "signed_32bit", 
-        "unsigned_32bit", 
-        "signed_64bit", 
-        "unsigned_64bit", 
-        "double", 
-        "void_ptr", 
-        "ptr", 
-        "str", 
-        "array", 
-        "container", 
-        "bindata", 
-        "undefined_type"
+    yield API(ch.inner.name), ch.inner.address  
 
-        ]
-    
-    #Convert datatype value from str to int e.g. "value": "0x0" -> "value" : 0x0
-    for i, param_data in enumerate(Param):
-        param = Param #does an instance of Param need to be made? param = Param(**param_data)
-        if Param.type in datatype:
-            try:
-                Param.value = int(Param.value)
-                print(f"Converted value to int for Param {i+1}: {Param.value}")
-            except ValueError:
-                print(f"Could not convert value '{Param.value}' to int for Param {i+1}")
-    print(Param)
-    
-    
     # Extract arguments from <in>
-    if FunctionCall.in_ is not None:
-        for param in FunctionCall.in_:
-            value = param.value
-            if value is not None:
-                if isinstance(value, str):
-                    yield String(value), Address(FunctionCall.address)
-                elif isinstance(value, int):
-                    yield Number(value), Address(FunctionCall.address)
-                else:
-                    assert_never(value)
+    for param in ch.inner.in_:
+        value = param.value
+        if isinstance(value, str):
+            yield String(value), ch.inner.address
+
+        elif isinstance(value, int):
+            yield Number(value), ch.inner.address
+
+        else:
+            assert_never(value)
 
     # Extract return value from <out>
-    if FunctionCall.out_ is not None:
-        for param in FunctionCall.out_:
-            value = param.value
-            if value is not None:
-                if isinstance(value, str):
-                    yield String(value), Address(FunctionCall.address)
-                elif isinstance(value, int):
-                    yield Number(value), Address(FunctionCall.address)
-                else:
-                    assert_never(value)
+    if ch.inner.out is not None:
+        value = ch.inner.out.value
+        if isinstance(value, str):
+            yield String(value), ch.inner.address
 
-def extract_features(
-    ph: ProcessHandle, th: ThreadHandle, ch: CallHandle
-) -> Iterator[Tuple[Feature, Address]]:
-    for handler in CALL_HANDLERS:
-        for feature, addr in handler(ph, th, ch):
-            yield feature, addr
+        elif isinstance(value, int):
+            yield Number(value), ch.inner.address
 
+        else:
+            assert_never(value)
 
-CALL_HANDLERS = (
-    extract_function_calls,
-    extract_features,)
+def extract_features(analysis: Analysis) -> Iterator[Tuple[Feature, Address]]:
+    '''
+    Extract features from the Analysis object in models.py
+    '''
+    for fncall in analysis.fncalls:
+        yield from extract_function_calls(fncall)
--- capa/features/extractors/vmray/models.py
+++ capa/features/extractors/vmray/models.py
@@ -6,205 +6,62 @@
 #  is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-from typing import Dict, List, Optional
+from typing import Any, Dict, List, Union, Literal, Optional
+from pydantic_xml import BaseXmlModel, attr, element
 
-from pydantic import BaseModel, Field
-
-
-# models flog.xml files
-class Param(BaseModel):
-    name: str
-    type: str
-    value: str
-
-class In_Out(BaseModel):
-    param: Param
-    
-class FunctionCall(BaseModel):
-    ts: str
-    fncall_id: str
-    process_id: str
-    thread_id: str
-    name: str
-    addr: str
-    from_addr: str = Field(alias="from")
-    in_: Optional[In_Out] = None
-    out: Optional[In_Out] = None
-
-class FunctionReturn(BaseModel):
-    ts: str
-    fncall_id: str
-    addr: str
-    from_addr: str = Field(alias="from")
+#
+class Param(BaseXmlModel):
+    name: str = attr()
+    type: str = attr()
+    value: str = attr()
 
 
-class Analysis(BaseModel):
-    log_version: str
-    analyzer_version: str
-    analysis_date: str
+class FunctionCall(BaseXmlModel, tag="fncall"):
+    ts: int = attr()
+    fncall_id: int = attr()
+    process_id: int = attr()
+    name: str = attr() #API call name?
+    address: str = attr() #address
+    from_: str = attr() 
+    in_: List[Param] = element(name="in")
+    out: Optional[Param] = element(name="out")
 
-    function_calls: List[FunctionCall] = Field(alias="fncall", default=[])
-    function_returns: List[FunctionReturn] = Field(alias="fnret", default=[])
+class FunctionReturn(BaseXmlModel, tag="fnret"):
+    ts: int = attr()
+    fncall_id: int = attr()
+    addr: str = attr() #string that contains a hex value
+    from_: str = attr #string that contains a hex value
 
-
-class Flog(BaseModel):
-    analysis: Analysis
-
-
-# models for summary_v2.json files
-class GenericReference(BaseModel):
-    path: List[str]
-    source: str
-
-
-class StaticDataReference(GenericReference):
-    ...
-
-
-class PEFileBasicInfo(BaseModel):
-    # compile_time: str
-    file_type: str
-    image_base: int
-    machine_type: str
-    # size_of_code: int
-    # size_of_initialized_data: int
-    # size_of_uninitialized_data: int
-    # subsystem: str
-    # entry_point: int
-    # imphash: Optional[str] = None
+class MonitorProcess(BaseXmlModel, tag="monitor_process"):
+    ts: int = attr()
+    process_id: int = attr()
+    image_name: str = attr()
 
 
-class API(BaseModel):
-    name: str
-    ordinal: Optional[int] = None
+class MonitorThread(BaseXmlModel, tag="monitor_thread"):
+    ts: int = attr()
+    thread_id: int = attr()
+    process_id: int = attr()
+    os_tid: str = attr()  # TODO hex
 
+class NewRegion(BaseXmlModel):
+    ts: int = attr()
+    start_va: str = attr()
+    end_va: str = attr()
+    entry_point: str = attr()
 
-class PEFileExport(BaseModel):
-    address: int
-    api: API
+class RemoveRegion(BaseXmlModel, tag="remove_region"):
+    ts: int = attr()
+    region_id: int = attr()
 
-
-class PEFileImport(BaseModel):
-    address: int
-    api: API
-    # thunk_offset: int
-    # hint: Optional[int] = None
-    # thunk_rva: int
-
-
-class PEFileImportModule(BaseModel):
-    dll: str
-    apis: List[PEFileImport]
-
-
-class PEFileSection(BaseModel):
-    # entropy: float
-    # flags: List[str] = []
-    name: str
-    # raw_data_offset: int
-    # raw_data_size: int
-    virtual_address: int
-    # virtual_size: int
-
-
-class PEFile(BaseModel):
-    basic_info: PEFileBasicInfo
-    exports: List[PEFileExport] = []
-    imports: List[PEFileImportModule] = []
-    sections: List[PEFileSection] = []
-
-
-class StaticData(BaseModel):
-    pe: Optional[PEFile] = None
-
-
-class FileHashes(BaseModel):
-    md5: str
-    sha1: str
-    sha256: str
-    # ssdeep: str
-
-
-class File(BaseModel):
-    # categories: List[str]
-    hash_values: FileHashes
-    # is_artifact: bool
-    #is_ioc: bool
-    is_sample: bool
-    # size: int
-    # is_truncated: bool
-    # mime_type: Optional[str] = None
-    # operations: List[str] = []
-    # ref_filenames: List[GenericReference] = []
-    # ref_gfncalls: List[GenericReference] = []
-    ref_static_data: Optional[StaticDataReference] = None
-    # ref_vti_matches: List[GenericReference] = []
-    # verdict: str
-
-
-class Process(BaseModel):
-    # bitness: int
-    # is_artifact: bool
-    #is_ioc: bool
-    monitor_id: int
-    # monitor_reason: str
-    os_pid: int
-    filename: str
-    image_name: str
-    ref_parent_process: Optional[GenericReference] = None
-
-
-class Filename(BaseModel):
-    filename: str
-    # is_artifact: bool
-    #is_ioc: bool
-    # verdict: str
-
-
-class Mutex(BaseModel):
-    name: str
-    # is_artifact: bool
-    #is_ioc: bool
-    # verdict: str
-
-
-class Registry(BaseModel):
-    reg_key_name: str
-    # reg_key_value_type: Optional[str] = None
-    # is_artifact: bool
-    #is_ioc: bool
-    # verdict: str
-
-
-class Domain(BaseModel):
-    domain: str
-    # is_artifact: bool
-    #is_ioc: bool
-    # verdict: str
-
-
-class IPAddress(BaseModel):
-    ip_address: str
-    # is_artifact: bool
-    #is_ioc: bool
-    # verdict: str
-
-
-class AnalysisMetadata(BaseModel):
-    sample_type: str
-    submission_filename: str
-
-
-class SummaryV2(BaseModel):
-    analysis_metadata: AnalysisMetadata
-
-    static_data: Dict[str, StaticData] = {}
-
-    # recorded artifacts
-    files: Dict[str, File] = {}
-    processes: Dict[str, Process] = {}
-    filenames: Dict[str, Filename] = {}
-    mutexes: Dict[str, Mutex] = {}
-    domains: Dict[str, Domain] = {}
-    ip_addresses: Dict[str, IPAddress] = {}
-    registry_records: Dict[str, Registry] = {}
+class Analysis(BaseXmlModel, tag="analysis"):
+    log_version: str = attr()
+    analyzer_version: str = attr()
+    analysis_date: str = attr()
+    processes: List[MonitorProcess] = element(tag="monitor_process")
+    threads: List[MonitorThread] = element(tag="monitor_thread")
+    new_regions: List[NewRegion] = element(tag="new_region")
+    remove_regions: List[RemoveRegion] = element(tag="remove_region")
+    fncalls: List[FunctionCall] = element(tag="fncall")
+    fnrets: List[FunctionReturn] = element(tag="fnret")
+   
