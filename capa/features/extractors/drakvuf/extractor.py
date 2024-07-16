8e7bc75bfb2946d0747f73e681f47d7752204a88
Yacine Elhamer <elhamer.yacine@gmail.com>
2024-07-16 15:00:32 +0100

    drakvuf/extractor.py: black linting

--- capa/features/extractors/drakvuf/extractor.py
+++ capa/features/extractors/drakvuf/extractor.py
@@ -81,9 +81,7 @@ class DrakvufExtractor(DynamicFeatureExtractor):
         call_name = "{}({}){}".format(
             call.name,
             ", ".join(f"{arg_name}={arg_value}" for arg_name, arg_value in call.arguments.items()),
-            (
-                f" -> {getattr(call, 'return_value', '')}"
-            ),  # SysCalls don't have a return value, while WinApi calls do
+            (f" -> {getattr(call, 'return_value', '')}"),  # SysCalls don't have a return value, while WinApi calls do
         )
         return call_name
 
