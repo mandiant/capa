62701a2837ba2deb95db82574657927b87ac711a
Soufiane Fariss <rev.fariss@gmail.com>
2024-07-30 17:38:40 +0200

    use Hash-Based routing (#)

--- webui/src/router/index.js
+++ webui/src/router/index.js
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import ImportView from '../views/ImportView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
