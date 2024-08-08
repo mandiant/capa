97bd4992b16817edfa204471a882593768b962e0
Soufiane Fariss <rev.fariss@gmail.com>
2024-08-08 07:06:41 +0200

    add path resolving shortcuts "@"

--- web/explorer/src/components/DescriptionPanel.vue
+++ web/explorer/src/components/DescriptionPanel.vue
@@ -2,7 +2,7 @@
     <div class="flex flex-column align-items-center">
         <div class="text-center">
             <h1>
-                <img src="../assets/images/logo-full.png" alt="Capa: identify program capabilities" />
+                <img src="@/assets/images/logo-full.png" alt="Capa: identify program capabilities" />
                 <h6 class="font-medium" style="color: rgb(176, 26, 26)">capa: identify program capabilities</h6>
             </h1>
         </div>
--- web/explorer/vite.config.js
+++ web/explorer/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { viteSingleFile } from "vite-plugin-singlefile";
+import { fileURLToPath, URL } from "node:url";
 
 // eslint-disable-next-line no-unused-vars
 export default defineConfig(({ command, mode }) => {
@@ -8,6 +9,12 @@ export default defineConfig(({ command, mode }) => {
 
     return {
         base: isBundle ? "/" : "/capa/",
-        plugins: isBundle ? [vue(), viteSingleFile()] : [vue()]
+        plugins: isBundle ? [vue(), viteSingleFile()] : [vue()],
+        resolve: {
+            alias: {
+                "@": fileURLToPath(new URL("src", import.meta.url)),
+                "@testfiles": fileURLToPath(new URL("../../tests/data", import.meta.url))
+            }
+        }
     };
 });
