09bbe80dfbba0827ff9d318f31848f483eca347e
Soufiane Fariss <rev.fariss@gmail.com>
2024-08-12 14:52:14 +0200

    add download button to download release

--- web/explorer/src/components/NavBar.vue
+++ web/explorer/src/components/NavBar.vue
@@ -8,10 +8,15 @@ import Menubar from "primevue/menubar";
             <div class="flex align-items-center gap-3">
                 <a
                     v-ripple
-                    href="https://github.com/mandiant/capa"
-                    class="flex align-items-center justify-content-center text-color w-2rem"
+                    v-tooltip.right="'Download capa Explorer Web for offline usage'"
+                    href="/capa-explorer-web.zip"
+                    download="capa-explorer-web.zip"
+                    aria-label="Download capa Explorer Web release"
                 >
-                    <i id="gitsub-icon" class="pi pi-github text-2xl"></i>
+                    <i class="pi pi-download text-xl"></i>
+                </a>
+                <a v-ripple href="https://github.com/mandiant/capa" class="flex justify-content-center w-2rem">
+                    <i class="pi pi-github text-2xl"></i>
                 </a>
                 <img src="@/assets/images/icon.png" alt="Logo" class="w-2rem" />
             </div>
