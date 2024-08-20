033b791082d19d4aea192e79f0a5db76504652ac
Willi Ballenthin <wballenthin@google.com>
2024-08-20 08:30:55 +0000

    web: rules: use pygments to syntax highlight rules
    Use the Pygments syntax-highlighting library to parse
    and render the YAML rule content. This way we don't have
    to manually traverse the rule nodes and emit lists; instead,
    we rely on the fact that YAML is pretty easy for humans
    to read and let them consume it directly, with some text 
    formatting to help hint at the types/structure.

--- web/rules/scripts/build_rules.py
+++ web/rules/scripts/build_rules.py
@@ -4,6 +4,10 @@ import os
 from glob import glob
 from datetime import datetime
 
+import pygments
+from pygments.lexers import YamlLexer
+from pygments.formatters import HtmlFormatter
+
 input_directory = sys.argv[1]
 txt_file_path = sys.argv[2]
 output_directory = sys.argv[3]
@@ -12,79 +16,47 @@ assert os.path.exists(input_directory), "input directory must exist"
 assert os.path.exists(txt_file_path), "file-modification txt file must exist"
 assert os.path.exists(output_directory), "output directory must exist"
 
-def render_features(features_list, indent=0):
-    rendered = ''
-    indent_str = ' ' * indent
-    if isinstance(features_list, list):
-        for item in features_list:
-            if isinstance(item, dict):
-                for key, value in item.items():
-                    if isinstance(value, (dict, list)):
-                        rendered += f'{indent_str}- {key}:\n'
-                        rendered += render_features(value, indent + 2)
-                    else:
-                        rendered += f'{indent_str}- {key}: {value}\n'
-            elif isinstance(item, list):
-                rendered += render_features(item, indent)
-            else:
-                rendered += f'{indent_str}- {item}\n'
-    elif isinstance(features_list, dict):
-        for key, value in features_list.items():
-            if isinstance(value, (dict, list)):
-                rendered += f'{indent_str}- {key}:\n'
-                rendered += render_features(value, indent + 2)
-            else:
-                rendered += f'{indent_str}- {key}: {value}\n'
-    else:
-        rendered += f'{indent_str}- {features_list}\n'
-    return rendered
-
-def render_list_item(key, value, indent=0):
-    indent_str = ' ' * indent
-    rendered = ''
-    if isinstance(value, list):
-        for item in value:
-            if isinstance(item, dict):
-                for subkey, subvalue in item.items():
-                    rendered += render_list_item(subkey, subvalue, indent)
-            else:
-                rendered += f'{indent_str}  - {item}\n'
-    elif isinstance(value, dict):
-        for subkey, subvalue in value.items():
-            rendered += render_list_item(subkey, subvalue, indent)
-    else:
-        rendered += f'{indent_str}- {key}: {value}\n'
-    return rendered
-
-def get_last_edited_date(file_path):
-    # TODO(williballenthin): use file_modification_dates.txt
-    last_modified_date = os.path.getmtime(file_path)
-    return datetime.fromtimestamp(last_modified_date)
 
 def convert_yaml_to_html(timestamps, yaml_file, output_dir):
-    with open(yaml_file, 'r') as file:
-        data = yaml.safe_load(file)
+    with open(yaml_file, 'rt', encoding="utf-8") as f:
+        rule_content = f.read()
+        f.seek(0)
+        # TODO(wb): load via capa
+        data = yaml.safe_load(f)
 
     rule = data.get('rule', {}).get('meta', {})
 
     namespace = rule.get('namespace', '')
-    authors = rule.get('authors', [])
-    scope_static = rule.get('scopes', {}).get('static', '')
-    scope_dynamic = rule.get('scopes', {}).get('dynamic', '')
-    attack = rule.get('att&ck', [])
-    mbc = rule.get('mbc', [])
-    references = ', '.join(rule.get('references', []))
-    examples = rule.get('examples', [])
-    features = data.get('rule', {}).get('features', [])
-
+    
     last_edited_str = timestamps[yaml_file]
 
-    rendered_features = render_features(features)
+    rendered_rule = pygments.highlight(
+       rule_content, 
+       YamlLexer(), 
+       HtmlFormatter(
+         style="xcode", 
+         noclasses=True,
+         wrapcode=True,
+         nobackground=True,
+       ))
 
-    name = rule.get('name', 'N/A')
+    name = rule['name']
+    # TODO(williballenthin): use capa for this
     sanitized_name = name.lower().replace(' ', '-').replace('/', '-').replace('\\', '-')
     html_file_name = f"{sanitized_name}.html"
 
+    # TODO(wb): link to GitHub source
+    # TODO(wb): link to ATT&CK
+    # TODO(wb): link to MBC
+    # TODO(wb): link to VT search
+    # TODO(wb): link to capa result examples
+    #
+    # TODO(wb): link to namespace
+    # TODO(wb): link to author
+    # TODO(wb): link references
+    # TODO(wb): link to examples
+
+    # TODO(wb): use jinja for templating
     html_content = f"""
     <!DOCTYPE html>
     <html lang="en">
@@ -94,49 +66,26 @@ def convert_yaml_to_html(timestamps, yaml_file, output_dir):
         <title>{name}</title>
         <link rel="icon"       href="./img/favicon.ico" type="image/x-icon">
         <link rel="stylesheet" href="./css/bootstrap-5.3.3.min.css">
-        <link rel="stylesheet" href="./pagefind/pagefind-ui.css">
-        <link rel="stylesheet" href="./pagefind/pagefind-modular-ui.css">
         <link rel="stylesheet" type="text/css" href="./css/styles.css">
+        <script src="./js/jquery-3.5.1.slim.min.js"></script>
+        <script src="./js/bootstrap-5.3.3.min.js"></script>
     </head>
     <body>
         <nav class="navbar navbar-light bg-light justify-content-between">
             <a class="navbar-brand" href="#">
                 <img src="./img/logo.png" alt="Logo" style="max-height: 65px;">
             </a>
-            <div id="search"></div>
         </nav>
-        <section id="showcase">
-            <div class="container">
-                <h1>{name}</h1>
-            </div>
-        </section>
-        <div class="container">
-            <div class="buttons">
-                <button>Last edited: {last_edited_str}</button>
-            </div>
-            <div class="card">
-                <div><b>Namespace:</b> {namespace}</div>
-                <div><b>Authors:</b></div><div class="grey-box">{render_list_item('Authors', authors)}</div>
-                <div><b>Scope:</b></div><div class="grey-box"><b>Static:</b> {scope_static}<br><b>Dynamic:</b> {scope_dynamic}</div>
-                <div><b>ATT&CK:</b></div><div class="grey-box">{render_list_item('ATT&CK', attack)}</div>
-                <div><b>MBC:</b></div><div class="grey-box">{render_list_item('MBC', mbc)}</div>
-                <div><b>References:</b> {references}</div>
-                <div><b>Examples:</b></div><div class="grey-box">{render_list_item('Examples', examples)}</div>
-                <div><b>Features:</b></div><div class="grey-box">{rendered_features}</div>
+        <div class="container d-flex justify-content-center">
+            <div style="max-width: 650px;">
+                <p class="lead mb-0 text-secondary">{namespace}</p>
+                <h1 class="display-6">{name}</h1>
+                <div class="mt-4">
+                    {rendered_rule}
+                </div>
+                <p class="text-secondary">last edited: {last_edited_str}</p>
             </div>
         </div>
-        <script src="./js/jquery-3.5.1.slim.min.js"></script>
-        <script src="./js/bootstrap-5.3.3.min.js"></script>
-        <script src="./pagefind/pagefind-ui.js" type="text/javascript"></script>
-        <script>
-            document.addEventListener('DOMContentLoaded', function() {{
-                new PagefindUI({{
-                    element: "#search",
-                    showEmptyFilters: false,
-                    excerptLength: 15
-                }});
-            }});
-        </script>
     </body>
     </html>
     """
