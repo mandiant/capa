df7697db8406a41c5222ebf61ce6a74dba7d8e8a
Ana Maria Martinez Gomez <anamg@google.com>
2025-01-15 13:05:24 +0100

    [copyright + license] Add missing headers
    Add copyright and license information headers to the source code files
    inside the `web` directory and the `capa/render/proto/capa.proto` file.
    I have used addlicense to add the headers.

--- web/explorer/src/components/misc/VTIcon.vue
+++ web/explorer/src/components/misc/VTIcon.vue
@@ -1,3 +1,19 @@
+<!--
+ Copyright 2024 Google LLC
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+-->
+
 <template>
     <svg width="14" height="14" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path d="M10.87 12L0 22.68h24V1.32H0zm10.73 8.52H5.28l8.637-8.448L5.28 3.48H21.6z" fill="currentColor" />
