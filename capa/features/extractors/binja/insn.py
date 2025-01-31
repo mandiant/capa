7361c849ceece6c97dbce9807540e7f7acb6444e
dhruvak001 <b22ai017@iitj.ac.in>
2025-01-31 21:15:06 +0530

    code style error

--- capa/features/extractors/binja/insn.py
+++ capa/features/extractors/binja/insn.py
@@ -14,9 +14,7 @@
 
 from typing import Any, Iterator, Optional
 
-from binaryninja import (
-    Function,
-)
+from binaryninja import Function
 from binaryninja import BasicBlock as BinjaBasicBlock
 from binaryninja import (
     BinaryView,
