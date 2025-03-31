237c9ef45b7037346c6f1f2ceef2223792e2865a
vibhatsu <maulikbarot2915@gmail.com>
2025-03-31 23:34:24 +0530

    refactor extract_insn_number_feature to use is_zxor helper
    Signed-off-by: vibhatsu <maulikbarot2915@gmail.com>

--- capa/features/extractors/ghidra/insn.py
+++ capa/features/extractors/ghidra/insn.py
@@ -157,12 +157,11 @@ def extract_insn_number_features(fh: FunctionHandle, bb: BBHandle, ih: InsnHandl
         return
 
     if insn.getMnemonicString().startswith("XOR"):
-        # for patern like:
-        #
-        #   xor eax, eax
-        if insn.getNumOperands() == 2:
-            if insn.getOpObjects(0)[-1] == insn.getOpObjects(1)[-1]:
-                yield Number(0), ih.address
+        if capa.features.extractors.ghidra.helpers.is_zxor(insn):
+            # for patern like:
+            #
+            #   xor eax, eax
+            yield Number(0), ih.address
 
     for i in range(insn.getNumOperands()):
         # Exceptions for LEA insn:
