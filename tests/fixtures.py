8a6814f3b8105b334633ebd0f9f97a2a2c1f8617
Mike Hunhoff <mike.hunhoff@gmail.com>
2026-01-15 18:47:13 +0000

    fix formatting

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -20,11 +20,11 @@ from functools import lru_cache
 
 import pytest
 
+import capa.loader
 import capa.features.file
 import capa.features.insn
 import capa.features.common
 import capa.features.basicblock
-import capa.loader
 from capa.features.common import (
     OS,
     OS_ANY,
