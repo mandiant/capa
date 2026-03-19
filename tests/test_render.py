2ed29b5526997670af6bb8b40ea691f604453503
blenbot <harshitiszz23@gmail.com>
2026-03-19 22:10:16 +0530

    tests: import capa.render.default in test_render
    Signed-off-by: blenbot <harshitiszz23@gmail.com>

--- tests/test_render.py
+++ tests/test_render.py
@@ -22,6 +22,7 @@ import capa.rules
 import capa.render.utils
 import capa.features.file
 import capa.features.insn
+import capa.render.default
 import capa.features.common
 import capa.features.freeze
 import capa.render.vverbose
@@ -29,7 +30,6 @@ import capa.features.address
 import capa.features.basicblock
 import capa.render.result_document
 import capa.render.result_document as rd
-import capa.render.default
 import capa.features.freeze.features
 from capa.render.utils import Console
 
