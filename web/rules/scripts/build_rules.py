0d55270a84a731864ed4f8378da229c4acb4ecf6
Mike Hunhoff <mike.hunhoff@gmail.com>
2026-04-07 18:34:03 +0000

    ci: fix web rules failure

--- web/rules/scripts/build_rules.py
+++ web/rules/scripts/build_rules.py
@@ -127,7 +127,7 @@ def render_rule(timestamps, path: Path) -> str:
     return html_content
 
 
-yaml_files = [str(p) for p in input_directory.glob("**/*.yml")]
+yaml_files = [str(p) for p in input_directory.glob("**/*.yml") if ".github" not in p.parts]
 
 timestamps = {}
 for line in txt_file_path.read_text(encoding="utf-8").splitlines():
