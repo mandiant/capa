9ba497f6f727791ca30fdd4daefb2244a985980e
Willi Ballenthin <wballenthin@hex-rays.com>
2026-05-11 08:52:47 +0200

    idalib: remove custom idalib loading

--- capa/features/extractors/ida/idalib.py
+++ capa/features/extractors/ida/idalib.py
@@ -12,13 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-import sys
-import json
 import logging
 import importlib.util
-from typing import Optional
-from pathlib import Path
 
 logger = logging.getLogger(__name__)
 
@@ -28,97 +23,3 @@ def is_idalib_installed() -> bool:
         return importlib.util.find_spec("idapro") is not None
     except ModuleNotFoundError:
         return False
-
-
-def get_idalib_user_config_path() -> Optional[Path]:
-    """Get the path to the user's config file based on platform following IDA's user directories."""
-    # derived from `py-activate-idalib.py` from IDA v9.0 Beta 4
-
-    if sys.platform == "win32":
-        # On Windows, use the %APPDATA%\Hex-Rays\IDA Pro directory
-        config_dir = Path(os.getenv("APPDATA")) / "Hex-Rays" / "IDA Pro"
-    else:
-        # On macOS and Linux, use ~/.idapro
-        config_dir = Path.home() / ".idapro"
-
-    # Return the full path to the config file (now in JSON format)
-    user_config_path = config_dir / "ida-config.json"
-    if not user_config_path.exists():
-        return None
-    return user_config_path
-
-
-def find_idalib() -> Optional[Path]:
-    config_path = get_idalib_user_config_path()
-    if not config_path:
-        logger.error("IDA Pro user configuration does not exist, please make sure you've installed idalib properly.")
-        return None
-
-    config = json.loads(config_path.read_text(encoding="utf-8"))
-
-    try:
-        ida_install_dir = Path(config["Paths"]["ida-install-dir"])
-    except KeyError:
-        logger.error(
-            "IDA Pro user configuration does not contain location of IDA Pro installation, please make sure you've installed idalib properly."
-        )
-        return None
-
-    if not ida_install_dir.exists():
-        return None
-
-    libname = {
-        "win32": "idalib.dll",
-        "linux": "libidalib.so",
-        "linux2": "libidalib.so",
-        "darwin": "libidalib.dylib",
-    }[sys.platform]
-
-    if not (ida_install_dir / "ida.hlp").is_file():
-        return None
-
-    if not (ida_install_dir / libname).is_file():
-        return None
-
-    idalib_path = ida_install_dir / "idalib" / "python"
-    if not idalib_path.exists():
-        return None
-
-    if not (idalib_path / "idapro" / "__init__.py").is_file():
-        return None
-
-    return idalib_path
-
-
-def has_idalib() -> bool:
-    if is_idalib_installed():
-        logger.debug("found installed IDA idalib API")
-        return True
-
-    logger.debug("IDA idalib API not installed, searching...")
-
-    idalib_path = find_idalib()
-    if not idalib_path:
-        logger.debug("failed to find IDA idalib installation")
-
-    logger.debug("found IDA idalib API: %s", idalib_path)
-    return idalib_path is not None
-
-
-def load_idalib() -> bool:
-    try:
-        import idapro
-
-        return True
-    except ImportError:
-        idalib_path = find_idalib()
-        if not idalib_path:
-            return False
-
-        sys.path.append(idalib_path.absolute().as_posix())
-        try:
-            import idapro  # noqa: F401 unused import
-
-            return True
-        except ImportError:
-            return False
